76d7f8a6bf939a0c2177b79593e64078e5dd448f
SaiSubi <saisubi.1652000@gmail.com>
2025-07-22 13:27:14 +0530

    Non-personalised Frontend

--- vibeai-ui/src/App.jsx
+++ vibeai-ui/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
 import Home from './pages/Home'
 import Results from './pages/Results'
+import HomeGen from './pages/HomeGen'
+import ResultsGen from './pages/ResultsGen'
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
         <Route path="/results" element={<Results />} />
+        <Route path="/homegen" element={<HomeGen />} />
+        <Route path="/resultsgen" element={<ResultsGen />} />
       </Routes>
     </Router>
   )
